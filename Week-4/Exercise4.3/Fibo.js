eb4f6bd893c0ecc3c92d9770ea2cf88b1e759b33
Anant Bajpai <anant.bajpai009@gmail.com>
2022-10-30 00:23:52 +0530

    Final Submit

---
+++ Week-4/Exercise4.3/Fibo.js
@@ -0,0 +1,21 @@
+let fibObj = {
+
+    one: 0, 
+    two: 1, 
+    temp: 0, 
+
+     [Symbol.iterator](){ 
+        return this; 
+     }, 
+
+    next(){ 
+        this.temp = this.two; 
+        this.two = this.temp + this.one; 
+        this.one = this.temp; 
+        return {value: this.two} 
+    } 
+} 
+
+for(let I = 0 ; I < 10; I++){ 
+       console.log(fibObj.next().value)
+} 
